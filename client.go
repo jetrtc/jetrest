042049146e1cad1bda2a2b4479587ee94c38f40d
JetRTC <jetrtc@gmail.com>
2019-11-18 00:57:10 +0800

    Client: extends capability to use io.Reader and bytes as request body and fix req/res dump

--- client.go
+++ client.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"strings"
 	"time"
 
@@ -13,15 +15,27 @@ import (
 	"github.com/jetrtc/log"
 )
 
+const (
+	contentTypeHeader = "Content-Type"
+	jsonContentType   = "application/json"
+	protoContentType  = "application/protobuf"
+)
+
 type Auth interface {
 	Authorize(req *http.Request) error
-	Validate(res *http.Response) (bool, error)
+	Validate(res *Response) (bool, error)
 	Invalidate() error
 }
 
 type Request struct {
 	client *Client
 	url    *URL
+	header http.Header
+}
+
+func (r *Request) Header(name, value string) *Request {
+	r.header.Set(name, value)
+	return r
 }
 
 func (r *Request) Join(path string) *Request {
@@ -36,7 +50,7 @@ func (r *Request) Param(name, value string) *Request {
 
 func (r *Request) Do(method string, v interface{}) (*Response, error) {
 	url := r.url.Encode()
-	return r.client.Request(method, url, v)
+	return r.client.request(method, r.header, url, v)
 }
 
 func (r *Request) Get() (*Response, error) {
@@ -75,7 +89,7 @@ func (r *Response) Unmarshal(val interface{}) error {
 	default:
 		err = json.Unmarshal(r.Body, val)
 	}
-	if err != nil && !protobuf && r.Response.Header.Get("Content-Type") != "application/json" {
+	if err != nil && !protobuf && r.Response.Header.Get(contentTypeHeader) != jsonContentType {
 		err = fmt.Errorf("%s", r.Body)
 	}
 	return err
@@ -106,7 +120,7 @@ func (c *Client) Protobuf() *Client {
 }
 
 func (c *Client) New(u string) *Request {
-	return &Request{client: c, url: NewURL(u)}
+	return &Request{client: c, url: NewURL(u), header: make(http.Header)}
 }
 
 func (c *Client) Get(url string) (*Response, error) {
@@ -127,12 +141,12 @@ func (c *Client) Delete(url string) (*Response, error) {
 
 func (c *Client) Request(method, url string, r interface{}) (*Response, error) {
 	start := time.Now()
-	res, err := c.request(method, url, r)
+	res, err := c.request(method, nil, url, r)
 	if err != nil {
 		return nil, err
 	}
 	if c.auth != nil {
-		valid, err := c.auth.Validate(res.Response)
+		valid, err := c.auth.Validate(res)
 		if err != nil {
 			c.Errorf("Failed to validate auth: %s", err.Error())
 			return nil, err
@@ -143,7 +157,7 @@ func (c *Client) Request(method, url string, r interface{}) (*Response, error) {
 				c.Errorf("Failed to invalidate auth: %s", err.Error())
 				return nil, err
 			}
-			res, err = c.request(method, url, r)
+			res, err = c.request(method, nil, url, r)
 			if err != nil {
 				return nil, err
 			}
@@ -153,31 +167,53 @@ func (c *Client) Request(method, url string, r interface{}) (*Response, error) {
 	return res, nil
 }
 
-func (c *Client) request(method, url string, r interface{}) (*Response, error) {
+func (c *Client) request(method string, header http.Header, url string, r interface{}) (*Response, error) {
 	var body []byte
 	var err error
 	protobuf := false
-	if r != nil {
-		switch r := r.(type) {
-		case proto.Message:
-			if c.protobuf {
-				body, err = proto.Marshal(r)
-				protobuf = true
-			} else {
-				body, err = json.Marshal(r)
-			}
-		default:
-			body, err = json.Marshal(r)
-		}
+	isJson := false
+	switch v := r.(type) {
+	case io.Reader:
+		body, err = ioutil.ReadAll(v)
 		if err != nil {
-			c.Errorf("Failed to marshal: %s", err.Error())
+			c.Errorf("Failed to read request body: %s", err.Error())
 			return nil, err
+		}
+	default:
+		t := reflect.ValueOf(r)
+		if t.Kind() == reflect.Slice && t.Type() == reflect.TypeOf([]byte(nil)) {
+			body = r.([]byte)
+		} else {
+			if r != nil {
+				switch r := r.(type) {
+				case proto.Message:
+					if c.protobuf {
+						protobuf = true
+						body, err = proto.Marshal(r)
+					} else {
+						isJson = true
+						body, err = json.Marshal(r)
+					}
+				default:
+					isJson = true
+					body, err = json.Marshal(r)
+				}
+				if err != nil {
+					c.Errorf("Failed to marshal: %s", err.Error())
+					return nil, err
+				}
+			}
 		}
 	}
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
 	if err != nil {
 		c.Errorf("Failed to create request: %s", err.Error())
 		return nil, err
+	}
+	if header != nil {
+		for k, v := range header {
+			req.Header[k] = v
+		}
 	}
 	if c.auth != nil {
 		auth := c.auth
@@ -192,18 +228,18 @@ func (c *Client) request(method, url string, r interface{}) (*Response, error) {
 	}
 	if body != nil && len(body) > 0 {
 		if protobuf {
-			req.Header.Set("Content-Type", "application/protobuf")
-		} else {
-			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set(contentTypeHeader, protoContentType)
+		} else if isJson {
+			req.Header.Set(contentTypeHeader, jsonContentType)
 		}
 	} else {
 		if c.protobuf {
-			req.Header.Set("Accept", "application/protobuf")
-		} else {
-			req.Header.Set("Accept", "application/json")
+			req.Header.Set("Accept", protoContentType)
+		} else if isJson {
+			req.Header.Set("Accept", jsonContentType)
 		}
 	}
-	c.dumpRequest(req, r, body)
+	c.dumpRequest(req, body, isJson)
 	res, err := c.client.Do(req)
 	if err != nil {
 		c.Errorf("Failed to make request: %s", err.Error())
@@ -215,7 +251,7 @@ func (c *Client) request(method, url string, r interface{}) (*Response, error) {
 		c.Errorf("Failed to read body: %s", err.Error())
 		return nil, err
 	}
-	c.dumpResponse(res, data, nil)
+	c.dumpResponse(res, data)
 	res.Body = ioutil.NopCloser(bytes.NewBuffer(data))
 	return &Response{
 		Response: res,
@@ -224,21 +260,19 @@ func (c *Client) request(method, url string, r interface{}) (*Response, error) {
 	}, nil
 }
 
-func (c *Client) dumpRequest(req *http.Request, v interface{}, data []byte) {
+func (c *Client) dumpRequest(req *http.Request, data []byte, isJson bool) {
 	dump := &struct {
 		Method   string                 `json:"method"`
 		URL      string                 `json:"url"`
 		Protocol string                 `json:"protocol"`
 		Headers  map[string]interface{} `json:"headers"`
 		Body     interface{}            `json:"body,omitempty"`
-		Data     []byte                 `json:"data,omitempty"`
 	}{
 		Method:   req.Method,
 		URL:      req.URL.RequestURI(),
 		Protocol: req.Proto,
 		Headers:  make(map[string]interface{}),
-		Body:     v,
-		Data:     data,
+		Body:     data,
 	}
 	for k, v := range req.Header {
 		if len(v) == 1 {
@@ -247,30 +281,33 @@ func (c *Client) dumpRequest(req *http.Request, v interface{}, data []byte) {
 			dump.Headers[k] = v
 		}
 	}
+	if isJson {
+		dump.Body = json.RawMessage(data)
+		bytes, err := json.Marshal(dump)
+		if err == nil {
+			c.Debugf("%s", bytes)
+			return
+		} else {
+			dump.Body = data
+		}
+	}
 	bytes, err := json.Marshal(dump)
-	if err != nil {
+	if err == nil {
 		c.Debugf("%s", bytes)
 	}
 }
 
-func (c *Client) dumpResponse(res *http.Response, data []byte, v interface{}) {
+func (c *Client) dumpResponse(res *http.Response, data []byte) {
 	dump := &struct {
 		Status   string                 `json:"status"`
 		Protocol string                 `json:"protocol"`
 		Headers  map[string]interface{} `json:"headers"`
 		Body     interface{}            `json:"body,omitempty"`
-		Data     []byte                 `json:"data,omitempty"`
 	}{
 		Status:   res.Status,
 		Protocol: res.Proto,
 		Headers:  make(map[string]interface{}),
-		Data:     data,
-	}
-	ctype := res.Header.Get("Content-Type")
-	if v != nil && (strings.HasPrefix(ctype, "application/json") || strings.HasPrefix(ctype, "application/protobuf")) {
-		dump.Body = v
-	} else {
-		dump.Body = string(data)
+		Body:     data,
 	}
 	for k, v := range res.Header {
 		if len(v) == 1 {
@@ -279,8 +316,20 @@ func (c *Client) dumpResponse(res *http.Response, data []byte, v interface{}) {
 			dump.Headers[k] = v
 		}
 	}
+	ctype := res.Header.Get(contentTypeHeader)
+	if strings.HasPrefix(ctype, jsonContentType) {
+		dump.Body = json.RawMessage(data)
+		bytes, err := json.Marshal(dump)
+		if err == nil {
+			c.Debugf("%s", bytes)
+			return
+		} else {
+			dump.Body = data
+		}
+	}
 	bytes, err := json.Marshal(dump)
-	if err != nil {
+	if err == nil {
 		c.Debugf("%s", bytes)
+		return
 	}
 }
